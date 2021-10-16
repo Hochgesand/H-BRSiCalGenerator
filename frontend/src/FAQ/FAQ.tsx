f7819f83e8e3e9fa87acd30815d56a967874f77b
André Schmitz <andreaabbcc@gmail.com>
2021-10-17 00:03:28 +0200

    WHOOPS forgot to install new dependencies in cicd

--- frontend/src/FAQ/FAQ.tsx
+++ frontend/src/FAQ/FAQ.tsx
@@ -11,15 +11,15 @@ export default function FAQ(){
       </button>
       <div className={"mt-5"}>
         <p className={"text-3xl"}>Beispiel 1: "Ich bin bei Herr Berrendorf in EidiP und gehöre der Gruppe D an.</p>
-        <img src={process.env.PUBLIC_URL + "/FUiTCuS2BJ.gif"}/>
+        <img src={process.env.PUBLIC_URL + "/FUiTCuS2BJ.gif"} alt={""}/>
       </div>
       <div className={"mt-5"}>
         <p className={"text-3xl"}>Beispiel 2: "Hilfe ich sehe nicht den ganzen Text!</p>
-        <img src={process.env.PUBLIC_URL + "/aTkfaQusHi.gif"}/>
+        <img src={process.env.PUBLIC_URL + "/aTkfaQusHi.gif"} alt={""}/>
       </div>
       <div className={"mt-5"}>
         <p className={"text-3xl"}>Beispiel 3: "Is ja alles schön und toll, aber was mache ich jetzt mit dieser komischen calendar.ics Datei (Beispiel an Google Kalender)"</p>
-        <img src={process.env.PUBLIC_URL + "/Z795yqtZO4.gif"}/>
+        <img src={process.env.PUBLIC_URL + "/Z795yqtZO4.gif"} alt={""}/>
       </div>
     </div>
   );
