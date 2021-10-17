7e9ef3bdafdf9bebbbb889e3b70b179d75d9e43d
André Schmitz <andreaabbcc@gmail.com>
2021-10-17 14:16:04 +0200

    Usability is jetzt deutlich besser

--- frontend/src/FAQ/FAQ.tsx
+++ frontend/src/FAQ/FAQ.tsx
@@ -5,10 +5,36 @@ export default function FAQ(){
   return(
     <div className={"p-5 flex-col gap-3"}>
       <button
-        className="btn btn-lg"
+        className="btn btn-lg mb-4"
         onClick={() => history.goBack()}>
         Back
       </button>
+      <div className={"rounded-box p-4 bg-base-300 flex-grow-0 w-1/2"}>
+        <p className={"text-3xl"}>Anleitung:</p>
+        <p className={"text-2xl"}>Schritt 1:</p>
+        <p>Wähle dir einfach alle Veranstaltungen aus die du besuchen möchtest.
+          Du kannst die Tabelle nach belieben sortieren und mit dem Burgerbutton kannst du auch gezielt
+          nach Keywords suchen. Wenn du alle Veranstaltungen ausgewählt hast... </p>
+        <p className={"text-2xl"}>Schritt 2: </p>
+        <p>Jetzt hast du zwei Möglichkeiten:
+          1.: Klicke auf "Hol dir deinen Kalender" und importiere es wie in Beispiel 3 einfach selber am Rechner.
+          2.: Tippe deine E-Mail Adresse ein und lasse es dir bequem per E-Mail Schicken. Klicke dafür einfach nachdem
+           du deine E-Mail Adresse eingetragen hast auf "Schicks per E-Mail".
+        </p>
+        <p className={"text-2xl"}>Misc:</p>
+        <p>Packages und Technologien die ich benutzt habe:</p>
+        <p>Spring Boot (Java):</p>
+        <ul>
+          <li> - Apache POI um Exceltabellen zu parsen</li>
+          <li> - ical4j um den Calender zu generieren</li>
+        </ul>
+        <p>React:</p>
+        <ul>
+          <li> - AGGrid für die Tabelle, kein Bock gehabt selber Sortieralgorithmen zu implementieren</li>
+          <li> - Locker noch ca. 2000 Packages mehr die mit React gekommen sind, npm macht npm sachen</li>
+        </ul>
+      </div>
+
       <div className={"mt-5"}>
         <p className={"text-3xl"}>Beispiel 1: "Ich bin bei Herr Berrendorf in EidiP und gehöre der Gruppe D an.</p>
         <img src={process.env.PUBLIC_URL + "/FUiTCuS2BJ.gif"} alt={""}/>
@@ -21,6 +47,7 @@ export default function FAQ(){
         <p className={"text-3xl"}>Beispiel 3: "Is ja alles schön und toll, aber was mache ich jetzt mit dieser komischen calendar.ics Datei (Beispiel an Google Kalender)"</p>
         <img src={process.env.PUBLIC_URL + "/Z795yqtZO4.gif"} alt={""}/>
       </div>
+
     </div>
   );
 }
--- frontend/src/actualApp/VeranstaltungsContainer.tsx
+++ frontend/src/actualApp/VeranstaltungsContainer.tsx
@@ -105,63 +105,48 @@ export default function VeranstaltungsContainer() {
       <div className={"grid grid-cols-3 mb-4 max-h-96 gap-4"}>
         <div className={"col-span-2 2xl:col-span-1 rounded-box p-4 bg-base-300 flex-grow-0"}>
           <h2 className={"text-4xl mb-2"}>H-BRS Kalendergenerator</h2>
-          <p>Anleitung: Wähle dir einfach alle Veranstaltungen aus die du besuchen möchtest.
-            Du kannst die Tabelle nach belieben sortieren und mit dem Burgerbutton kannst du auch gezielt
-            nach Keywords suchen. Wenn du alle Veranstaltungen ausgewählt hast klick einfach
-            auf den Button "Hol dir deinen Kalender!" Du kriegst deinen Kalender als .ics Datei ausgegeben,
-            diese Datei kannst du einfach in der Kalendersoftware deiner Wahl importieren.
-            Wenn du nicht weißt wie das geht, google es für deine Software.</p>
+          <p>Die Seite befindet sich noch in der Beta Phase, Fehler bitte melden! (a(at)andrevr.de)</p>
           <p>Ich übernehme keine Haftung für die Richtigkeit der generierten Daten, alles nach bestem Wissen und
             Gewissen.</p>
-          <p>Die Seite befindet sich noch in der Beta Phase, Fehler bitte melden! (a(at)andrevr.de)</p>
-        </div>
-
-        <div className={"rounded-box p-4 bg-base-300 flex-grow-0"}>
-          <h3>Packages und Technologien die ich benutzt habe:</h3>
-          <p>Spring Boot (Java):</p>
-          <ul>
-            <li>Apache POI um Exceltabellen zu parsen</li>
-            <li>ical4j um den Calender zu generieren</li>
-          </ul>
-          <p>React:</p>
-          <ul>
-            <li>AGGrid für die Tabelle, kein Bock gehabt selber Sortieralgorithmen zu implementieren</li>
-            <li>Locker noch ca. 2000 Packages mehr die mit React gekommen sind, npm macht npm sachen</li>
-          </ul>
         </div>
       </div>
 
-      <div className={"ag-theme-alpine-dark rounded-box p-3 bg-base-300"} ag-grid="gridOptions" style={{height: 800}}>
+      <div className={"grid grid-rows-2 mt-4 gap-4 xl:w-9/12 mb-4"}>
+        <div className={"grid grid-cols-2 gap-4 w-5/12 rounded-box p-3 bg-base-300"}>
+          <button onClick={onButtonClickDownloadCalendar} className={"btn btn-lg"}>Hol dir deinen Kalender!</button>
+          <a href={"https://github.com/Hochgesand/H-BRSiCalGenerator"} target="_blank" rel="noopener noreferrer">
+            <button className={"btn btn-lg"}>Gib mir einen Stern auf Github ❤</button>
+          </a>
+        </div>
+        <div className={"grid grid-cols-3 gap-4 w-1/2 rounded-box p-3 bg-base-300 flex-row"}>
+          <input
+            className="appearance-none w-auto bg-base-200 text-white border border-white rounded py-4 px-4 leading-tight focus:outline-none focus:bg-base-400 text-2xl"
+            id="grid-first-email" type="text" placeholder="E-Mail" onChange={e => setEmail(e.target.value)}
+          />
+          <button className={"btn btn-lg"} type={"submit"} onClick={onEmailWantToSchick}>Schicks per E-Mail</button>
+          <button className={"btn btn-lg"} onClick={handleClick}>HILFE!</button>
+        </div>
+      </div>
+
+      <div className={"ag-theme-alpine-dark rounded-box p-3 bg-base-300"} style={{height: 800}}>
         <AgGridReact
           rowData={veranstaltungsData}
           enableRangeSelection={true}
           rowSelection={"multiple"}
           ref={gridRef}
-          floatingFilter={true}
           rowMultiSelectWithClick={true}
           // @ts-ignore
           onFirstDataRendered={() => gridRef.current.api.sizeColumnsToFit()}
         >
           <AgGridColumn field="name" width={380} sortable={true} filter={true} checkboxSelection={true} resizable={true}
-                        headerName={"Veranstaltung"}/>
-          <AgGridColumn field="prof" width={150} sortable={true} filter={true} resizable={true}/>
+                        headerName={"Veranstaltung"} floatingFilter={true}/>
+          <AgGridColumn field="prof" width={150} sortable={true} filter={true} resizable={true}
+                        floatingFilter={true}/>
           <AgGridColumn field="studienGangSemester" width={400} sortable={true} filter={true} resizable={true}
-                        headerName={"Fachbereich / Semester"}/>
+                        floatingFilter={true} headerName={"Fachbereich / Semester"}/>
         </AgGridReact>
       </div>
-      <div className={"grid grid-rows-1 grid-cols-5 mt-4 gap-4 xl:w-9/12 "}>
-        <button onClick={onButtonClickDownloadCalendar} className={"btn btn-lg"}>Hol dir deinen Kalender!</button>
-        <a href={"https://github.com/Hochgesand/H-BRSiCalGenerator"} target="_blank" rel="noopener noreferrer">
-          <button className={"btn btn-lg"}>Gib mir einen Stern auf Github ❤</button>
-        </a>
-        <input
-          className="appearance-none w-auto bg-base-200 text-white border border-red-500 rounded py-4 px-4 leading-tight focus:outline-none focus:bg-base-400 text-2xl"
-          id="grid-first-email" type="text" placeholder="E-Mail" onChange={e => setEmail(e.target.value)}
-        />
-        <button className={"btn btn-lg"} type={"submit"} onClick={() => onEmailWantToSchick()}>Schicks per E-Mail</button>
-        <button className={"btn btn-lg"} onClick={() => handleClick()}>HILFE!</button>
-        <button className={"btn btn-lg"} >DIE E-MAIL WIRD RAUSGEHEN, ICH HAB NUR NOCH KEIN FEEDBACK IMPLEMENTIERT!!!</button>
-      </div>
+
     </div>
   );
 
