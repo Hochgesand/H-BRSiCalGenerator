0c721ce53dcdc523bd7c54df64f4d1e1cef49693
Kevin Gath <kevin@hgath.de>
2021-10-27 10:02:34 +0200

    Replace response object with native types

--- backend/src/main/java/student/aschm22s/hbrsiCalGenerator/hbrsiCalGenerator/controller/CalenderExportController.java
+++ backend/src/main/java/student/aschm22s/hbrsiCalGenerator/hbrsiCalGenerator/controller/CalenderExportController.java
@@ -1,18 +1,15 @@
 package student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.repository.VeranstaltungsRepo;
 import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.models.DAOObjects.VeranstaltungsIds;
 import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.models.DAOObjects.VeranstaltungsIdsAndEmail;
 import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.models.Veranstaltung;
+import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.repository.VeranstaltungsRepo;
 import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.service.CalenderGeneratorService;
 import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.service.EmailSendingService;
 
-import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -30,8 +27,48 @@ public class CalenderExportController {
 
     String[] blacklistedEMails = new String[]{"a@andrevr.de", "moin@meister.ovh"};
 
+    @RequestMapping(value = "/getVeranstaltungen", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Veranstaltung> getVeranstaltungen() {
+        return veranstaltungsRepo.findAll();
+    }
+
+    @RequestMapping(
+            value = "/sememesteriCalEmail",
+            method = {RequestMethod.POST},
+            produces = "text/calender"
+    )
+    public String getCalenderOverEmail(@RequestBody VeranstaltungsIdsAndEmail veranstaltungsIdsAndEmail) {
+        if (Arrays.stream(blacklistedEMails).anyMatch(x -> veranstaltungsIdsAndEmail.getEmail().equals(x))) {
+            return "Fühlst dich wohl witzig du Penner.";
+        }
+
+        return emailSendingService.getCalenderOverEmail(veranstaltungsIdsAndEmail);
+    }
+
+    @RequestMapping(
+            value = "/sememesteriCal",
+            method = {RequestMethod.POST},
+            produces = "text/calender"
+    )
+    public byte[] getCalenderForSemester(@RequestBody VeranstaltungsIds veranstaltungsIds) throws IOException {
+        return calenderGeneratorService.createCalender(veranstaltungsIds);
+    }
+
+    @RequestMapping(
+            value = "/sememesteriCalAsCSV",
+            method = {RequestMethod.POST},
+            produces = "text/csv"
+    )
+    public String getCalenderForSemesterAsCSV(@RequestBody VeranstaltungsIds veranstaltungsIds) throws IOException {
+        return calenderGeneratorService.createCalenderAsCSV(veranstaltungsIds);
+    }
+
     class veranstaltungsIds {
         List<Integer> veranstaltungsIds;
+
+        public veranstaltungsIds(List<Integer> veranstaltungsIds) {
+            this.veranstaltungsIds = veranstaltungsIds;
+        }
 
         public List<Integer> getVeranstaltungsIds() {
             return veranstaltungsIds;
@@ -40,73 +77,5 @@ public class CalenderExportController {
         public void setVeranstaltungsIds(List<Integer> veranstaltungsIds) {
             this.veranstaltungsIds = veranstaltungsIds;
         }
-
-        public veranstaltungsIds(List<Integer> veranstaltungsIds) {
-            this.veranstaltungsIds = veranstaltungsIds;
-        }
-    }
-
-    @RequestMapping(value = "/getVeranstaltungen", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends Object> getVeranstaltungen() {
-        List<Veranstaltung> veranstaltungen = (List<Veranstaltung>) veranstaltungsRepo.findAll();
-        return new ResponseEntity<List<Veranstaltung>>(
-                veranstaltungen,
-                HttpStatus.OK
-        );
-    }
-
-    @RequestMapping(value = "/getVeranstaltungen", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends Object> getSpecificVeranstaltungen(@RequestParam(value = )) {
-        List<Veranstaltung> veranstaltungen = (List<Veranstaltung>) veranstaltungsRepo.findAll();
-        return new ResponseEntity<List<Veranstaltung>>(
-                veranstaltungen,
-                HttpStatus.OK
-        );
-    }
-
-    @RequestMapping(
-            value = "/sememesteriCalEmail",
-            method = {RequestMethod.POST},
-            produces = "text/calender"
-    )
-    public ResponseEntity<? extends Object> getCalenderOverEmail(@RequestBody VeranstaltungsIdsAndEmail veranstaltungsIdsAndEmail) throws MessagingException, IOException {
-        if (Arrays.stream(blacklistedEMails).anyMatch(x -> veranstaltungsIdsAndEmail.getEmail().equals(x))) {
-            return new ResponseEntity<String>(
-                    "Fühlst dich wohl witzig du Penner.",
-                    HttpStatus.OK
-            );
-        }
-
-        return new ResponseEntity<String>(
-                emailSendingService.getCalenderOverEmail(veranstaltungsIdsAndEmail),
-                HttpStatus.OK
-        );
-    }
-
-    @RequestMapping(
-            value = "/sememesteriCal",
-            method = {RequestMethod.POST},
-            produces = "text/calender"
-    )
-    public ResponseEntity<? extends Object> getCalenderForSemester(@RequestBody VeranstaltungsIds veranstaltungsIds) throws IOException {
-        return new ResponseEntity<>(
-                calenderGeneratorService.createCalender(veranstaltungsIds),
-                HttpStatus.OK
-        );
-
-    }
-
-
-    @RequestMapping(
-            value = "/sememesteriCalAsCSV",
-            method = {RequestMethod.POST},
-            produces = "text/csv"
-    )
-    public ResponseEntity<? extends Object> getCalenderForSemesterAsCSV(@RequestBody VeranstaltungsIds veranstaltungsIds) throws IOException {
-        return new ResponseEntity<>(
-                calenderGeneratorService.createCalenderAsCSV(veranstaltungsIds),
-                HttpStatus.OK
-        );
-
     }
 }
--- backend/src/main/java/student/aschm22s/hbrsiCalGenerator/hbrsiCalGenerator/repository/VeranstaltungsRepo.java
+++ backend/src/main/java/student/aschm22s/hbrsiCalGenerator/hbrsiCalGenerator/repository/VeranstaltungsRepo.java
@@ -1,14 +1,18 @@
 package student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import student.aschm22s.hbrsiCalGenerator.hbrsiCalGenerator.models.Veranstaltung;
 
 @Repository
-public interface VeranstaltungsRepo extends CrudRepository<Veranstaltung, Integer> {
+public interface VeranstaltungsRepo extends JpaRepository<Veranstaltung, Integer> {
     Iterable<Veranstaltung> findByName(String name);
+
     Iterable<Veranstaltung> findByStudienGangSemester(String studienGangSemester);
+
     Iterable<Veranstaltung> findByIdIn(Iterable<Integer> Ids);
+
     Veranstaltung findFirstByNameAndStudienGangSemester(String name, String studienGangSemester);
+
     void deleteAllByStudienGangSemester(String studienGangSemester);
 }
