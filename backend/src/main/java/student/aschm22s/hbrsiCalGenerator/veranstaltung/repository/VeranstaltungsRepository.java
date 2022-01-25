fcfa0f508705c07f0613c206f59a50c1ad9939b7
André Schmitz <andre.schmitz@lise.de>
2022-01-25 17:19:22 +0100

    updated Backend to get only events for specific "studiengang"

--- backend/src/main/java/student/aschm22s/hbrsiCalGenerator/veranstaltung/repository/VeranstaltungsRepository.java
+++ backend/src/main/java/student/aschm22s/hbrsiCalGenerator/veranstaltung/repository/VeranstaltungsRepository.java
@@ -4,9 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import student.aschm22s.hbrsiCalGenerator.veranstaltung.domain.Veranstaltung;
 
+import java.util.List;
+
 @Repository
 public interface VeranstaltungsRepository extends JpaRepository<Veranstaltung, Integer> {
     Iterable<Veranstaltung> findByIdIn(Iterable<Integer> Ids);
+
+    List<Veranstaltung> findAllByStudienGangSemester(String studienGangSemester);
+
+    List<Veranstaltung> findAllByStudienGangSemesterContaining(String studienGangSemester);
 
     Veranstaltung findFirstByNameAndStudienGangSemester(String name, String studienGangSemester);
 
--- backend/src/main/java/student/aschm22s/hbrsiCalGenerator/veranstaltung/service/VeranstaltungsService.java
+++ backend/src/main/java/student/aschm22s/hbrsiCalGenerator/veranstaltung/service/VeranstaltungsService.java
@@ -6,12 +6,14 @@ import student.aschm22s.hbrsiCalGenerator.stundenplan.service.StundenplanService
 import student.aschm22s.hbrsiCalGenerator.veranstaltung.domain.Veranstaltung;
 import student.aschm22s.hbrsiCalGenerator.veranstaltung.repository.VeranstaltungsRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class VeranstaltungsService {
     private final VeranstaltungsRepository veranstaltungsRepository;
     private final StundenplanService stundenplanService;
+    List<String> studiengaenge = new ArrayList<>();
 
     public VeranstaltungsService(VeranstaltungsRepository veranstaltungsRepository, StundenplanService stundenplanService) {
         this.veranstaltungsRepository = veranstaltungsRepository;
@@ -20,6 +22,29 @@ public class VeranstaltungsService {
 
     public List<Veranstaltung> findAll() {
         return veranstaltungsRepository.findAll();
+    }
+
+    public List<Veranstaltung> findAllByStudiengang(String studiengang) {
+        studiengang = studiengang.substring(0, studiengang.length()-2);
+        return veranstaltungsRepository.findAllByStudienGangSemesterContaining(studiengang);
+    }
+
+    public List<String> findAllStudiengaenge() {
+        if (studiengaenge.size() != 0)
+            return studiengaenge;
+
+        Iterable<Veranstaltung> veranstaltungen = veranstaltungsRepository.findAll();
+
+        for (Veranstaltung x :
+                veranstaltungen) {
+            x.setStudienGangSemester(x.getStudienGangSemester().substring(0, x.getStudienGangSemester().length() - 2));
+
+            if (!studiengaenge.contains(x.getStudienGangSemester())){
+                studiengaenge.add(x.getStudienGangSemester());
+            }
+        }
+
+        return studiengaenge;
     }
 
     public void deleteAll() {
