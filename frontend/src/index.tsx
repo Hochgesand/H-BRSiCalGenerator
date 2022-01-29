bf0abc7453fdc83f270604e23240bf77802c201e
André Schmitz <andre.schmitz@lise.de>
2022-01-29 17:49:26 +0100

    Switched to an HashRouter

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
