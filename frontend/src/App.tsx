18fc325f6d80e9b0f47250c75ed5856162693e41
André Schmitz <andre.schmitz@lise.de>
2022-02-23 16:54:30 +0100

    Moved to amplify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.scss';
-import Lander from "./actualApp/Lander";
-import {Routes} from 'react-router-dom';
-import {Route} from "react-router-native";
-import FAQ from "./FAQ/FAQ";
-import VeranstaltungsSelector from "./actualApp/VeranstaltungsSelector/VeranstaltungsSelector";
 
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path={"/"} element={<Lander/>}/>
-                <Route path={"/H-BRSiCalGenerator"} element={<Lander/>}/>
-                <Route path={"/H-BRSiCalGenerator/FAQ"} element={<FAQ/>}/>
-                <Route path={"/H-BRSiCalGenerator/:studiengang"} element={<VeranstaltungsSelector/>}/>
-            </Routes>
+           <a href={"https://kalenderapp.aptinstall.de/"}><button className={"btn btn-lg"}>Der Kalendergenerator liegt jetzt hier :) Drück mich</button></a>
         </div>
     );
 }
