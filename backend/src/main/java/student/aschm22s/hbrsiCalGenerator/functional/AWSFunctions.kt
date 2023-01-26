8429edcdddb22c4926f36a4e85f03bdc92d8ec92
André Schmitz <andre.schmitz@lise.de>
2023-01-26 16:24:59 +0100

    Added AWS Lambda Support

--- backend/src/main/java/student/aschm22s/hbrsiCalGenerator/functional/AWSFunctions.kt
+++ backend/src/main/java/student/aschm22s/hbrsiCalGenerator/functional/AWSFunctions.kt
@@ -1,17 +1,32 @@
 package student.aschm22s.hbrsiCalGenerator.functional
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import student.aschm22s.hbrsiCalGenerator.calenderExport.service.CalenderExportService
 import student.aschm22s.hbrsiCalGenerator.email.service.EmailSendingService
+import student.aschm22s.hbrsiCalGenerator.stundenplanSpecific.studiengang.domain.Studiengang
+import student.aschm22s.hbrsiCalGenerator.stundenplanSpecific.studiengang.service.StudiengangService
+import student.aschm22s.hbrsiCalGenerator.stundenplanSpecific.veranstaltung.domain.Veranstaltung
 import student.aschm22s.hbrsiCalGenerator.stundenplanSpecific.veranstaltung.domain.VeranstaltungsIdsAndEmailDTO
+import student.aschm22s.hbrsiCalGenerator.stundenplanSpecific.veranstaltung.service.VeranstaltungsService
 import java.util.*
 
-class AWSFunctions(
+@Configuration
+open class AWSFunctions(
     private val calenderExportService: CalenderExportService,
-    private val emailSendingService: EmailSendingService
+    private val emailSendingService: EmailSendingService,
+    private val studiengangService: StudiengangService,
+    private val veranstaltungsService: VeranstaltungsService
 ) {
-    val blacklistedEMails = arrayOf("a@andrevr.de", "moin@meister.ovh")
+    private val blacklistedEMails = arrayOf("a@andrevr.de", "moin@meister.ovh")
 
-    fun getCalenderOverEmailFun(): (VeranstaltungsIdsAndEmailDTO) -> String {
+    @Bean
+    open fun getCalenderOverEmailFun(): (VeranstaltungsIdsAndEmailDTO) -> String {
         return fun(veranstaltungsIdsAndEmailDTO: VeranstaltungsIdsAndEmailDTO): String {
             return if (Arrays.stream(blacklistedEMails)
                     .anyMatch { x: String -> veranstaltungsIdsAndEmailDTO.email == x }
@@ -21,5 +36,24 @@ class AWSFunctions(
         }
     }
 
+    @Bean
+    open fun getAllStundiengaenge(): () -> List<Studiengang> {
+        return fun(): List<Studiengang> {
+            return studiengangService.findAllStudiengaenge()
+        }
+    }
 
+    @Bean
+    open fun getVeranstaltungen(): () -> List<Veranstaltung?>? {
+        return fun(): List<Veranstaltung>{
+            return veranstaltungsService.findAll()
+        }
+    }
+
+    @Bean
+    open fun getVeranstaltungByStudiengang(): (String) -> List<Veranstaltung> {
+        return fun (studiengang: String): List<Veranstaltung> {
+            return veranstaltungsService.findAllByStudiengang(studiengang)
+        }
+    }
 }
